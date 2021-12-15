353c9a3b0fd15524db4b5b688190aec142df2944
tamashun9 <tamashun9@gmail.com>
2021-12-15 19:10:25 +0900

    DB環境構築

--- database/migrations/2021_12_15_181344_add_game_id_to_posts_table.php
+++ database/migrations/2021_12_15_181344_add_game_id_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class AddGameIdToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('game_id')->unsigned();    //unsigned()型で定義
+            //'game_id' は 'gtameテーブル' の 'id' を参照する外部キーです
         });
     }
 
@@ -27,6 +26,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
